1152f10f0ab7d7b095306ec233f39f57ea1c389c
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2025-01-23 20:05:50 -0600

    added remining get, post, put, delete calls for recipes

--- recipeApi/obj/Release/net8.0/recipeApi.AssemblyInfo.cs
+++ recipeApi/obj/Release/net8.0/recipeApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("recipeApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c13ca63a1d46e026e763bfa362681ee69e1c7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("recipeApi")]
