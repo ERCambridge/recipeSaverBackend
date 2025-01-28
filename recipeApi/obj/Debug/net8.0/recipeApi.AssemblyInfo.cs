0575ac5f815f3bac677c6a4408d4b8de54ddb7d4
Vaxaldan <evankendall68@gmail.com>
2025-01-27 21:57:15 -0500

    added cors to back end

--- recipeApi/obj/Debug/net8.0/recipeApi.AssemblyInfo.cs
+++ recipeApi/obj/Debug/net8.0/recipeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recipeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c13ca63a1d46e026e763bfa362681ee69e1c7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28c226f37c75a09eebe95cca4533dfa14246fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("recipeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recipeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
