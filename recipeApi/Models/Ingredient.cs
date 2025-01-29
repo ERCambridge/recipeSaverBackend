87276c6257dfbe074ce95595be4815db33293fea
Ethan Cambridge <ethancambridge@hotmail.com>
2025-01-29 18:59:58 -0500

    added ingredient table to database and regenerated EF core to include it

---
+++ recipeApi/Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace recipeApi.Models;
+
+public partial class Ingredient
+{
+    public int IngredientId { get; set; }
+
+    public string IngredientName { get; set; } = null!;
+
+    public string IngredientQuantity { get; set; } = null!;
+
+    public int? UserId { get; set; }
+
+    public virtual User? User { get; set; }
+}
